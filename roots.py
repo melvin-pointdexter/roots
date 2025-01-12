0eed91f469c3c057cdcd9abdaf19afc1de18c38f
melvin-pointdexter <sagigl@ac.sce.ac.il>
2025-01-12 18:50:16 +0200

    add: complete bisection method

--- roots.py
+++ roots.py
@@ -1,15 +1,27 @@
 #Imports
 
+#Helper functions
+def polynomial(poly,val):
+    su=0
+    for power,comp in enumerate(poly):
+        su += comp*(val**(len(poly)-1-power))
+    return su
+
 #Functions
-def Bisection_Method():
+def Bisection_Method(poly, start, end, eps, iterations = 1):
+    mid=(start+end)/2
+    if (abs(polynomial(poly, mid)) < eps):
+        print("Calculation took", iterations, "iterations")
+        return mid
+    if (polynomial(poly, start)*polynomial(poly, mid) < 0):
+        return Bisection_Method(poly, start, mid, eps, iterations + 1)
+    return Bisection_Method(poly, mid, end, eps, iterations + 1)
+
+def Newton_Raphson(poly, start, end, eps, iterations = 1):
     #stub
     return
 
-def Newton_Raphson():
-    #stub
-    return
-
-def Secant_Method():
+def Secant_Method(poly, start, end, eps, iterations = 1):
     #stub
     return
 
@@ -17,7 +29,13 @@ def Secant_Method():
 
 #Main Function
 def main():
-    #stub
-    return
+    poly=[1,0,-3]
+    start, end = 1,2
+    eps = 0.00001
+    methods = { "1" : Bisection_Method, "2": Newton_Raphson, "3": Secant_Method}
+    print("Welcome to the program!\nEnter the method of choice:")
+    print("1 - Bisection, 2 - Newton Raphson, 3 - Secant")
+    print("The root is:", methods[input()](poly, start, end, eps))
+    return 0
 
 main()
