9437b19d66671f5f6c060723028a3d543e5dc4d1
melvin-pointdexter <sagigl@ac.sce.ac.il>
2025-01-12 15:29:10 +0200

    add stubs file

---
+++ roots.py
@@ -0,0 +1,23 @@
+#Imports
+
+#Functions
+def Bisection_Method():
+    #stub
+    return
+
+def Newton_Raphson():
+    #stub
+    return
+
+def Secant_Method():
+    #stub
+    return
+
+
+
+#Main Function
+def main():
+    #stub
+    return
+
+main()
